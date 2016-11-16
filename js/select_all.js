6ca66bed0552eca86c0cbc0bcaaade0d7bcfd612
JigneshSatam <jigneshsatam@gmail.com>
2016-11-17 01:50:30 +0530

    Infinite scroll fininshed

--- js/select_all.js
+++ js/select_all.js
@@ -8,44 +8,62 @@ if (typeof jQuery === "undefined") {
     var selectables;
     var settings = $.extend({
       class: "no_class",
-      infinite_select: false
+      infinite_scroll_select: false
     }, options );
 
     var select_all = $(this);
     var find_in = select_all;
 
-    select_all.addClass("select_all "+settings.class);
-
-    while( !find_in.is("body") ){
-      selectables = find_in.find(":checkbox.selectable");
-      if (selectables.length > 0){
-        selectables.addClass(settings.class);
-        find_in = $("body");
-      }
-      else{
-        find_in = find_in.parent();
-      }
+    select_all.addClass("select_all").attr("data-select_all_class", settings.class);
+    selectables = find_selectables(find_in);
+    if (selectables.length > 0){
+      selectables.addClass(settings.class);
     }
 
-    $(".select_all."+settings.class).change(function(){
+    $(".select_all[data-select_all_class='"+settings.class+"']").change(function(){
       $(".selectable."+settings.class).prop('checked', $(this).prop("checked"));
     });
-    $(".selectable."+settings.class).change(function(){
-      if ($(".selectable."+settings.class+":checked").length == $(".selectable."+settings.class).length)
-        $(".select_all."+settings.class).prop('checked', "checked");
-      else
-        $(".select_all."+settings.class).prop('checked', false);
-    });
-    if(settings.infinite_select){
-      debugger
-      $(select_all).data("total_child_count", selectables.length);
+    initialise_selectables(selectables);
+    if(settings.infinite_scroll_select){
+      $(select_all).attr("data-total_child_count", selectables.length);
       $(document).ajaxComplete(function( event, xhr, settings ){
-        $("[data='total_child_count']").each(function(){
-          $(this).data("total_child_count")
+        $("[data-total_child_count]").each(function(){
+          var elem = $(this);
+          var total_selectables = find_selectables(elem);
+          if (total_selectables.length > elem.data("total_child_count")){
+            total_selectables.addClass(elem.data("select_all_class"));
+            elem.attr("data-total_child_count", total_selectables.length);
+            initialise_selectables(total_selectables);
+            if(elem.prop("checked")){
+              total_selectables.prop('checked', true)
+            }
+          }
         });
-        console.log("dude");
-      })
+      });
     }
+
+    function find_selectables(find_in) {
+      var selectables, searched;
+      while( !find_in.is("body") ){
+        searched = find_in.find(":checkbox.selectable");
+        if (searched.length > 0){
+          selectables = searched;
+          find_in = $("body");
+        }
+        else{
+          find_in = find_in.parent();
+        }
+      }
+      return selectables;
+    }
+    function initialise_selectables(selectables){
+      $(selectables).change(function(){
+        if ($(".selectable."+settings.class+":checked").length == $(".selectable."+settings.class).length)
+          $(".select_all[data-select_all_class='"+settings.class+"']").prop('checked', "checked");
+        else
+          $(".select_all[data-select_all_class='"+settings.class+"']").prop('checked', false);
+      });  
+    }
+    return $(this);
   }
-  return $(this);
 }( jQuery ));
